c07f6227ec95c18eb0a0ea04442f9b251662610a
John_considine <johnconsidine23@yahoo.co.uk>
2022-11-01 21:07:22 +0000

    add functions for creationg grid and add letters to numbers dictionary

--- run.py
+++ run.py
@@ -1,3 +1,38 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+from random import randint
+
+def create_grids():
+    pass
+
+def print_board(board, game_size):
+    pass
+
+letters_to_numbers ={
+    'A': O,
+    'B': 1,
+    'C': 2,
+    'D': 3,
+    'E': 4,
+    'F': 5,
+    'G': 6,
+    'H': 7,
+    'I': 8,
+    'J': 9,
+    'K': 10,
+    'L': 11,
+    'M': 12,
+    'N': 13,
+    'O': 14,
+    'P': 15,
+    'Q': 16,
+    'R': 17,
+    'S': 18,
+    'T': 19,
+    'U': 20,
+    'V': 21,
+    'W': 22,
+    'X': 23,
+    'Y': 24,
+    'Z': 25
+}
+
+    
