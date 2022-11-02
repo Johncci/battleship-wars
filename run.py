cee3d582e530d33465715c8659b04a6e98ed7426
John_considine <johnconsidine23@yahoo.co.uk>
2022-11-02 20:23:35 +0000

    add function for outlining game grid

--- run.py
+++ run.py
@@ -1,12 +1,26 @@
 from random import randint
 
 def create_grids():
-    HIDDEN_BOARD = [[" "] * for x in range()]
-    GUESS_ BOARD = [[" "] * for i in range()]
+    HIDDEN_BOARD = [[" "] * game_size for x in range(game_size)]
+    GUESS_BOARD = [[" "] * game_size for i in range(game_size)]
 
-
+# credit second half of this function in readme
 def print_board(board, game_size):
     letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    top_labels = " "
+    seperator = " "
+    for letter in range(game_size):
+        top_labels += letters[letter]
+        seperator = "+-"
+
+    print(top_labels)
+    print(seperator)
+    
+    row_number = 1
+    for row in board:
+        print("%s|%s|" % (" "+str(row_number) if row_number <
+              10 else row_number, "|".join(row)))
+        row_number += 1
 
 letters_to_numbers ={
     'A': 0,
@@ -40,15 +54,28 @@ letters_to_numbers ={
 game_size = 0
 
 while game_size == 0 or game_size <= 7 or game_size > 26:
+    try:
+        game_size = int(input(
+            "Welcome to battleship, please enter a grid size from 8 and 26 keeping in mind the larger the grid the more difficult the game!"))
+        if game_size <= 7 or game_size > 26:
+            print("Invalid input, please enter a grid size from 8 and 26!")
+    except ValueError:
+        print("Invalid input, please enter a grid size from 8 and 26!")
+
 
 def create_ships():
-    pass
+    for ship in range(5):
+        ship_row, ship_column = randint(0, game_size-1), randint(0, game_size-1)
+        pass
 
 def get_ship_location():
     pass
 
 def count_hit_ships():
     pass
+
+create_grids(game_size)
+
 
 
 
